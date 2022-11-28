efc06c85daf2fd029481bb644763e259ddb28cbf
chenhui <chenhui@inke.cn>
2022-11-28 22:17:00 +0800

    1.微尘继续

---
+++ android/app/src/main/java/com/hangchen/native_method/HelloPlugin.java
@@ -0,0 +1,79 @@
+package com.hangchen.native_method;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+/** HelloPlugin */
+public class HelloPlugin implements FlutterPlugin, MethodCallHandler, ActivityAware {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private MethodChannel channel;
+
+  private static final String CHANNEL = "amples.chenhang/navigation";
+  private Context context;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL);
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    }
+    if (call.method.equals("openAppStore")) {
+      try {
+        Uri uri = Uri.parse("market://details?id=com.tencent.mm");
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(intent);
+      } catch (Exception e) {
+        result.error("UNAVAILABLE", "没有安装应用市场", null);
+      }
+    }
+    else {
+      result.notImplemented();
+    }
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+
+  @Override
+  public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
+     context = binding.getActivity();
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(@NonNull ActivityPluginBinding binding) {
+
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+
+  }
+}
--- android/app/src/main/java/com/hangchen/native_method/MainActivity.java
+++ android/app/src/main/java/com/hangchen/native_method/MainActivity.java
@@ -1,38 +1,87 @@
 package com.hangchen.native_method;
 
-import android.os.Bundle;
-import io.flutter.app.FlutterActivity;
+
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
 import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.PluginRegistry;
+//import io.flutter.plugins.GeneratedPluginRegistrant;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugins.GeneratedPluginRegistrant;
+
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-public class MainActivity extends FlutterActivity {
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    GeneratedPluginRegistrant.registerWith(this);
+import android.util.Log;
 
-    new MethodChannel(getFlutterView(), "samples.chenhang/navigation").setMethodCallHandler(
-            new MethodChannel.MethodCallHandler() {
-              @Override
-              public void onMethodCall(MethodCall call, MethodChannel.Result result) {
-                // Note: this method is invoked on the main thread.
-                if(call.method.equals("openAppStore")) {
-                  try {
-                    Uri uri = Uri.parse("market://details?id=com.tencent.mm");
-                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(intent);
-                  } catch (Exception e) {
-                    result.error("UNAVAILABLE", "没有安装应用市场", null);
-                  }
-                }
-                else {
-                  result.notImplemented();
-                }
-              }
-            });
-  }
+public class MainActivity extends FlutterActivity {
+
+    private static final String CHANNEL = "amples.chenhang/navigation";
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        GeneratedPluginRegistrant.registerWith(this);
+    }
+
+    //  @Override
+//  protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//    GeneratedPluginRegistrant.registerWith(this);
+//
+//    new MethodChannel(getFlutterView(), "samples.chenhang/navigation").setMethodCallHandler(
+//            new MethodChannel.MethodCallHandler() {
+//              @Override
+//              public void onMethodCall(MethodCall call, MethodChannel.Result result) {
+//                // Note: this method is invoked on the main thread.
+//                if(call.method.equals("openAppStore")) {
+//                  try {
+//                    Uri uri = Uri.parse("market://details?id=com.tencent.mm");
+//                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                    startActivity(intent);
+//                  } catch (Exception e) {
+//                    result.error("UNAVAILABLE", "没有安装应用市场", null);
+//                  }
+//                }
+//                else {
+//                  result.notImplemented();
+//                }
+//              }
+//            });
+//  }
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+
+        try {
+            flutterEngine.getPlugins().add(new HelloPlugin());
+        } catch(Exception e) {
+            Log.e(TAG, "Error registering plugin flutter_plugin_android_lifecycle, io.flutter.plugins.flutter_plugin_android_lifecycle.FlutterAndroidLifecyclePlugin", e);
+        }
+//        GeneratedPluginRegistrant.registerWith(flutterEngine);
+//        flutterEngine.
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler((call, result) -> {
+                            if (call.method.equals("openAppStore")) {
+                                try {
+                                    Uri uri = Uri.parse("market://details?id=com.tencent.mm");
+                                    Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                } catch (Exception e) {
+                                    result.error("UNAVAILABLE", "没有安装应用市场", null);
+                                }
+                            } else {
+                                result.notImplemented();
+                            }
+                        }
+                     );
+
+    }
 }
